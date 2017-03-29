a3ef2cdcc496729ea6b3f6d14a8f6ccb5972782e
Omar <omaru.lyl@gmail.com>
2017-03-28 20:38:00 -0600

    Se agrego menu offconvas y menu drilldown para moviles

--- js/app.js
+++ js/app.js
@@ -4,3 +4,11 @@ $('[data-toggle-dia]').click( function (ev) {
 	const panel = $(this).data('toggleDia')
 	$('#lineup-tabs').foundation('selectTab', panel)
 })
+
+const $offCanvas = $('#offCanvas')
+
+
+$offCanvas.find('li').click(function(ev){
+	// alert('hola')
+	$offCanvas.foundation('close')
+})
